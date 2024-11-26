a20aaa00419aa30ca68209499bfe4428ace63154
Your Name <khalidsaifullaha3@gmail.com>
2024-11-26 17:03:35 +0600

    Added coffee card and delete

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import { Link } from "react-router-dom";
+import { Link, useLoaderData } from "react-router-dom";
 import "./App.css";
+import CoffeeCard from "./components/CoffeeCard";
 
 function App() {
+  const coffees = useLoaderData();
+
   return (
-    <>
+    <div className="m-20">
       <div className=" flex justify-center items-center gap-4 mt-5">
         <Link to={"/addCoffee"}>
           <button className=" btn  border-pink-600">Add Coffee</button>
@@ -12,8 +15,15 @@ function App() {
           <button className=" btn  border-pink-600">Update Coffee</button>
         </Link>
       </div>
-      <h1 className="text-6xl text-purple-600">Vite + React</h1>
-    </>
+      <h1 className="text-6xl text-purple-600 text-center my-4">
+        Hot Hot Cold Coffee: {coffees.length}
+      </h1>
+      <div className="grid md:grid-cols-2 gap-5 border-t">
+        {coffees.map((coffee) => (
+          <CoffeeCard key={coffee._id} coffee={coffee}></CoffeeCard>
+        ))}
+      </div>
+    </div>
   );
 }
 
---
+++ src/components/CoffeeCard.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+import Swal from "sweetalert2";
+
+const CoffeeCard = ({ coffee }) => {
+  const { _id, name, quantity, supplier, taste, category, details, photo } =
+    coffee;
+
+  const handleDelete = (_id) => {
+    console.log(_id);
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        // Swal.fire({
+        //   title: "Deleted!",
+        //   text: "Your file has been deleted.",
+        //   icon: "success",
+        // });
+        fetch(`http://localhost:5000/coffee/${_id}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(data);
+            if (data.deletedCount > 0) {
+              Swal.fire({
+                title: "Deleted!",
+                text: "Your Coffee has been deleted.",
+                icon: "success",
+              });
+            }
+          });
+      }
+    });
+  };
+
+  return (
+    <div>
+      <div className="card card-side bg-base-100 shadow-xl">
+        <figure>
+          <img src={photo} alt="Movie" />
+        </figure>
+        <div className=" flex justify-between  w-full px-4 pt-3">
+          <div>
+            <h2 className="card-title">Name: {name}</h2>
+            <p>Chef: {supplier}</p>
+            <p>{taste}</p>
+          </div>
+
+          <div className="card-actions justify-end">
+            <div className="join join-vertical space-y-4">
+              <button className="btn join-item">View</button>
+              <button className="btn join-item">Edit</button>
+              <button
+                onClick={() => handleDelete(_id)}
+                className="btn join-item"
+              >
+                X
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CoffeeCard;
